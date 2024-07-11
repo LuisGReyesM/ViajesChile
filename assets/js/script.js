fd930eec0dd9dced441ac3ba282798b07ac355f8
GRoblivium <lgreyesmiranda@gmail.com>
2024-07-11 16:52:21 -0400

    Fix, agrego funcionalidad JQ y mejoro el README.md

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,7 +6,7 @@ $(document).ready(function () {
     $('#enviarCorreo').on('click', function (event) {
         event.preventDefault();
         alert('El correo fue enviado correctamente');
-        $('form')[0].reset();        
+        $('form')[0].reset();
     });
 
     // Toggle para mostrar/ocultar la clase 'card-clicked' al hacer clic en cualquier .card
@@ -25,4 +25,22 @@ $(document).ready(function () {
         $('.indicadores-carousel .indicador').removeClass('activo');
         $('.indicadores-carousel .indicador').eq(currentIndex).addClass('activo');
     });
+
+
+    // Seleccionar todas las columnas con la clase 'col-md-4' y agregar un evento click
+    $('.col-md-4').click(function () {
+        // Encontrar los elementos dentro de la columna
+        var customBox = $(this).find('.custom-box'); // Seleccionar el elemento con la clase 'custom-box' y se agrega a la variable
+        var paragraph = $(this).find('p'); // Seleccionar el párrafo dentro de la columna y se agrega a la variable
+
+        // Invertir su posición
+        if (customBox.next().is('p')) {
+            // Si el siguiente elemento después de 'customBox' es un párrafo, mover el párrafo antes de 'customBox'
+            paragraph.insertBefore(customBox);
+        } else {
+            // Si el siguiente elemento después de 'customBox' no es un párrafo, mover 'customBox' antes del párrafo
+            customBox.insertBefore(paragraph);
+        }
+    });
+
 });
